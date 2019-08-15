0ad870a8485b3d0941e81bb463f65a1261e1dd0a
Franky Liu <cuifranky95@gmail.com>
2019-08-15 22:36:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,9 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+  
       
     }, 
     :capulet => {
